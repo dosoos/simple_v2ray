a6c1ce58924a509531ccf3a3f2f63e9b1ca5f241
dmit <dosoos94@gmail.com>
2024-01-10 14:58:32 +0000

    update userlist

--- cli_user_list.sh
+++ cli_user_list.sh
@@ -3,13 +3,19 @@ CONFIG_FILE=$(dirname "${BASH_SOURCE[0]}")'/v2ray/config.json'
 for key in $(jq '.inbounds[0].settings.clients | keys | .[]' $CONFIG_FILE);
 do
 
-	EMAIL=$(eval "jq '.inbounds[0].settings.clients["$key"]' $CONFIG_FILE | grep email" | awk -F':' '{print $2}' | sed 's/[\", ]//g');
+	EMAIL=$(eval "jq -r '.inbounds[0].settings.clients["$key"][\"email\"]' $CONFIG_FILE");
 
 	echo $key
 	
 	echo $EMAIL
 
-    	echo $(eval "jq '.inbounds[0].settings.clients["$key"]' $CONFIG_FILE" | grep -E "id|level|alterId|updatetime" | sed 's/[", ]//g');
+    	echo 'id: '$(eval "jq -r '.inbounds[0].settings.clients["$key"][\"id\"]' $CONFIG_FILE");
+
+    	echo 'level: '$(eval "jq -r '.inbounds[0].settings.clients["$key"][\"level\"]' $CONFIG_FILE");
+
+    	echo 'alterId: '$(eval "jq -r '.inbounds[0].settings.clients["$key"][\"alterId\"]' $CONFIG_FILE");
+
+    	echo 'updatetime: '$(eval "jq -r '.inbounds[0].settings.clients["$key"][\"updatetime\"]' $CONFIG_FILE");
 
 	echo 'upload: '$(eval "docker exec v2ray v2ray api stats -regexp $EMAIL'.+uplink' | grep -e 'Total' || echo 'Total: 0B' " | sed 's/Total: //g' );
 
